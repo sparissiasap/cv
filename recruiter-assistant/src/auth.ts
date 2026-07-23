a6302e7cff631a19df00b117b6425e6236508b94
Sergio Parissi <38665286+sparissiasap@users.noreply.github.com>
2026-07-23 14:54:24 -0500

    Add gmail.modify scope for authentication

--- recruiter-assistant/src/auth.ts
+++ recruiter-assistant/src/auth.ts
@@ -5,6 +5,7 @@ import readline from "readline";
 const SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/gmail.send"
 ];
 
